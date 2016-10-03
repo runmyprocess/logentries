bc67e23a32bac47da44b17ec8b83cb3063cc491e
Grégoire Juge <gregoire.juge@gmail.com>
2016-10-03 19:26:54 +0200

    Remove config.py -> moved to $HOME

--- src/logentries/config.py
+++
@@ -1,5 +0,0 @@
-API_KEY = '4d7f41b2-02ac-40ad-af47-d805c931fb24'
-LOG_DICT = {
-    'live_jboss': 'bf17f887-5c08-4215-8ee4-54c3d5277c5f',
-    'beta_jboss': 'a69680e5-461e-42ad-8fb2-464505747ead'
-}
